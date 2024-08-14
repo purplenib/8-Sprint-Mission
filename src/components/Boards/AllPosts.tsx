794348cac0453df7fca5df2d5a26df72e54f27a3
DESKTOP-THOA91P\영주 <breadjoo1014@naver.com>
2024-08-14 23:27:50 +0900

    design: 글쓰기 버튼 높이 조정

--- src/components/Boards/AllPosts.tsx
+++ src/components/Boards/AllPosts.tsx
@@ -51,7 +51,11 @@ function AllPosts() {
     <section className="flex flex-col gap-6 ">
       <div className="flex flex-row justify-between h-[42px] items-center">
         <h1 className="text-gray-900 font-bold text-xl">게시글</h1>
-        <Button buttonText="글쓰기" />
+        <Button
+          buttonText="글쓰기"
+          href="/boards/${addBoards}"
+          className="h-full"
+        />
       </div>
       <div className="flex flex-row gap-3 max-xl:mt-12">
         <SearchForm
